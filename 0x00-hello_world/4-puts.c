071a174076c8dd01cf8dcfb05d5041774cc115f5
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-05 14:01:37 -0700

    sdfa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-main function is to print
+ *
+ * return always eequel zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
