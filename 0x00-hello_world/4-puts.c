1c85d58cb383321ea792787209ea155cc707c504
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-05 14:31:06 -0700

    sg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
- * main-main function is to print
+ * main - print "Programming is like building a multilingual puzzle"
+ *
+ * Description: C program that prints with put
  *
  * return: always 0 (succes)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
