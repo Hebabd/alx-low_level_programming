dc1d9ef732af2970ae05e3c663b5f2e8f37542c5
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-05 14:47:13 -0700

    fs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Description:A c programy that print with printf
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
