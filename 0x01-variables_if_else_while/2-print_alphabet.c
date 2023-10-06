767136981a6ae44ea783b7c3bef60c45896bdb41
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-06 02:50:45 -0700

    gdf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry pint
+ *
+ * Description:print all small alphapet letters
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
