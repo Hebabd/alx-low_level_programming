9914dcea734bf8c9d4675574d4fb3fc2735afde4
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-10 14:10:17 -0700

    fsd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10-function
+ *print_alphabet_x10 -function
  *
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int line, ch;
-	
+
 	for (line = 0; line <= 9; line++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}
+}
