9ab00ebb4616dd75a80cb530840b3d1b237b9f10
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-10 15:40:43 -0700

    sf

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - sum of two num
+ *
+ * @a: first num
+ * @b: second num
+ *
+ * Return: sum of a &b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
