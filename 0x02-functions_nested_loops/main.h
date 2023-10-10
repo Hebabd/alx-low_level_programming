845c35c9709ebe9b872609039b8b0050bbccb1f1
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-10 10:53:29 -0700

    ff

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,9 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char);
-void print_alphabet(void);
-void print_alhabet_x10(void);
-int _islower(int c);
-int print_sign(int n);
-_abs(int);
-print_last_digit(int);
-jack_bauer(void);
-times_table(void);
-add(int, int);
-void print_to_98(int n);
-print_times_table(int n);
+#include <stdio.h>
+
+int _putchar(char c);
+
 
 #endif
