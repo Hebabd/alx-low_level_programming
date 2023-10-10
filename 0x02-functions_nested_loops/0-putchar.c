c59d5e0265269e059e6979c85da42b8e6211955c
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-10 12:03:09 -0700

    fg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
-*main: print _putcher
-*
-*Description:print putcher
-*
-*Return:return always 0
-*/
+ * main-print _putcher
+ * is written
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_puthar('\n');
+	return (0);
 }
