3b70db19b08b124b8811f6a83f6c0c3fcd13e2d7
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-12 14:11:47 -0700

    fd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_lin- print a straight line
+ *
+ * @n: is the number of times
+ *
+ *
+ *
+ */
+
+void print_line(int n);
+{
+	int lin;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lin = 1; lin <= n; lin++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
