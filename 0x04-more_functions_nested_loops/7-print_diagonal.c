768dd692f241e0b35feddbc4de1751d169b6d7a5
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-12 14:23:31 -0700

    dgs

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prin a diagonal line
+ *
+ * @n: the number of times
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int post, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (post = 1; post <= n; post++)
+		{
+			for (space = 1; space <= post; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
