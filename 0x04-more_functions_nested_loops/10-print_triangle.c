7e8bed7a53a358260c91c997d72a47788faca85c
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-12 15:51:56 -0700

    dgs

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= size -i; j++)
+			for (j = 1; j <= size - i; j++)
 		{
 			_putchar(' ');
 		}
@@ -30,4 +30,4 @@ void print_triangle(int size)
 		_putchar('\n');
 		}
 	}
-}
+i}
