1b947ad655ff44acfaddaa36087aa76e1ed9e25e
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-12 15:24:39 -0700

    dgsS

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - print numbers 1 -100
+ * numbers that are multiples of 3 print Fizz
+ * numbers that are multiples of 5 print Buzz
+ * numbers that are multiples of 15 print FizzBuzz
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int unm;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num %  == 0 && !(num % 3 == 0))
+			printf("buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ")
+		else
+			printf("\n");
+	}
+	return (0);
+}
