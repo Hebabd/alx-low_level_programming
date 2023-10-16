c9e08e8077d238ab6627ae767f89ea7d0d145f75
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-16 12:31:07 -0700

    ggh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half -  print second haf of string
+ * @str: char
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
