d891a05b57f2acfa3c9a8fd985f3ba264f9455f4
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-16 11:53:37 -0700

    gd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints astring in reverse
+ *
+ * @s: pointe to the string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n')
+}
