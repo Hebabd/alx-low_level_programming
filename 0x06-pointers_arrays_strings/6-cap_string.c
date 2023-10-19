fafa1cb26ba5361a69e23fafb08553dbdf2d972d
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-19 13:11:02 -0700

    dg

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,57 +2,18 @@
 #include <stdio.h>
 
 /**
- * isLower - determines whether ascii is lowecase
+ * main - check the code
  *
- * @c: character
- * Return: 1 if true, 0 if false
+ * Return: Always 0.
  */
 
-int isLower(charc)
+int main(void)
 {
-	return (c >= 97 && c <= 122);
-}
+	char str[] = "Expect the best. Prepare for the worst.Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 
-/**
- * isDelimiter - determines wbether ascii is a delimiter
- * @c: character
- * Return: 1 if true, 0 if false
- */
-
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = " \t\n,.!?\"(){};
-
-	for (i = 0; i < 12; i++)
-		if(c == delimiter[i])
-			return (1);
+	char *ptr;
+		ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
-}
-
-/**
-* cap_string - capitalies all words of a string
-* @: input string
-* Return: string with capitalized words
-*/
-
-char *cap_string(char *s)
-{
-	char *ptr = s;
-	int foundDelimit = 1;
-
-	while (*S)
-	{
-		if (isDelimiter(*S))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
-		{
-			*S -= 32;
-			foundDelimit = 0;
-		}
-		else
-			foundDelimit = 0;
-		s++;
-	}
-	return (ptr);
 }
