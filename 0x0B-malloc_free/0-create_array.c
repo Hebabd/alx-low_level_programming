3fa9d8d4162b48959d40ab7fc67854556fddbdf6
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-10-30 01:37:56 -0700

    dg

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,15 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *n = malloc(sie);
+	char *str;
+	unsigned int i;
 
-	if (size == 0 || n == 0)
-	return (0);
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+	return (NULL);
 
-	while (size--)
-		n[size] = c;
+	for (i = 0; i < size; i++)
+		str[i] = c;
 
-		return (n);
+		return (str);
 }
