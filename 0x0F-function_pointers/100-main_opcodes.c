ea395646d275bb010ad7cf336ec7b646eb70bb6a
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-11-08 05:48:56 -0800

    hf

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,18 +9,40 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *p = (char *)main;
-	int b;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	b = atoi(argv[1]);
-	if (b < 0)
-		printf("Error\n"), exit(2);
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-	while (b--)
-		printf("%2hhx%s", *p++, b ? "" : "\n");
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes;
+			index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
 	return (0);
 }
