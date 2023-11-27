0d18006ab82904e16b2bbfb0be70300809e69365
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-11-27 09:11:58 -0800

    len

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,5 +39,5 @@ int create_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == llen ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
