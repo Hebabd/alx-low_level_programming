b5752d09a959715cf532a1e51972a129a7dc183d
Hebabd <hebatallahabdelhamid90@gmail.com>
2023-11-27 11:56:45 -0800

    g

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,12 +33,15 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 
-		return (-1);
-	fd = open(filename, )_WRONLY | O_APPEND);
+		return (0);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (-1);
+		return (0);
+
 	if (len)
 		bytes = write(fd, text_content, len);
+	
 	close(fd);
-	return (bytes == len ? 1 : -1);
+	return (bytes == len ? 1 : 0);
 }
